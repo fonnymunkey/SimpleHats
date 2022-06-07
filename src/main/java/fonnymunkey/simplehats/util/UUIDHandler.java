18121debb3dfaef337f3e3243317c1ae6e5205f2
fonnymunkey <fonnymunkey@gmail.com>
2022-06-07 01:19:46 -0400

    Testing initial UUID handling

--- src/main/java/fonnymunkey/simplehats/util/UUIDHandler.java
+++ src/main/java/fonnymunkey/simplehats/util/UUIDHandler.java
@@ -22,18 +22,17 @@ public class UUIDHandler {
     private static Map<String, Integer> uuidMap = new HashMap<>();
 
     public static void init(Path resourcePath) {
-        try {
+        try {//Check UUID registry on github, save to map
             URL url = new URL(uuidUrl);
             BufferedReader read = new BufferedReader(new InputStreamReader(url.openStream()));
             uuidMap = new Gson().fromJson(read, new TypeToken<Map<String, Integer>>(){}.getType());
             read.close();
-            System.out.println("Read UUID map");
         }
         catch(Exception ex) {
             SimpleHats.logger.log(Level.WARN, "Failed to fetch UUID file: " + ex);
             return;
         }
-        try {
+        try {//Compare existing UUID file to github UUID, if different download new uuid.json and resourcepack.zip
             boolean changed = false;
             File parent = resourcePath.resolve(SimpleHats.modId + "_hatdl").toFile();
             if(!parent.exists()) parent.mkdirs();
@@ -41,32 +40,21 @@ public class UUIDHandler {
             File zipFile = new File(parent, "simplehats_hatdl.zip");
 
             if(uuidFile.exists()) {
-                System.out.println("UUID exists");
                 String fileString = Files.asCharSource(uuidFile, Charset.defaultCharset()).read();
                 JsonObject json = JsonParser.parseString(fileString).getAsJsonObject();
                 Map<String, Integer> uuidMapPre = new Gson().fromJson(json, new TypeToken<Map<String, Integer>>(){}.getType());
-                changed = uuidMapPre.equals(uuidMap);
+                changed = !uuidMapPre.equals(uuidMap);
             }
             else changed = true;
 
             if(changed) {
-                System.out.println("UUID changed, dl'ing");
                 if(uuidFile.exists()) uuidFile.delete();
                 if(zipFile.exists()) zipFile.delete();
 
                 URL uuidURL = new URL(uuidUrl);
-                BufferedReader readUUID = new BufferedReader(new InputStreamReader(uuidURL.openStream()));
-                BufferedWriter writeUUID = new BufferedWriter(new FileWriter(uuidFile));
-                readUUID.transferTo(writeUUID);
-                readUUID.close();
-                writeUUID.close();
-
+                java.nio.file.Files.copy(uuidURL.openStream(), uuidFile.toPath());
                 URL zipURL = new URL(zipUrl);
-                BufferedReader readZIP = new BufferedReader(new InputStreamReader(zipURL.openStream()));
-                BufferedWriter writeZIP = new BufferedWriter(new FileWriter(zipFile));
-                readZIP.transferTo(writeZIP);
-                readZIP.close();
-                writeZIP.close();
+                java.nio.file.Files.copy(zipURL.openStream(), zipFile.toPath());
             }
         }
         catch(Exception ex) {
