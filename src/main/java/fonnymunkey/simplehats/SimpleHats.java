9366eb19caacb8f7dbcc3acf19e247993a7fded6
fonnymunkey <fonnymunkey@gmail.com>
2023-03-06 20:52:25 -0500

    0.1.7
    Readd creative tab and update lang

--- src/main/java/fonnymunkey/simplehats/SimpleHats.java
+++ src/main/java/fonnymunkey/simplehats/SimpleHats.java
@@ -4,18 +4,23 @@ import fonnymunkey.simplehats.common.entity.HatDisplay;
 import fonnymunkey.simplehats.common.init.HatJson;
 import fonnymunkey.simplehats.common.init.ModConfig;
 import fonnymunkey.simplehats.common.init.ModRegistry;
+import fonnymunkey.simplehats.common.item.HatItem;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.condition.KilledByPlayerLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,13 +29,9 @@ public class SimpleHats implements ModInitializer {
     public static Logger logger = LogManager.getLogger();
     public static ModConfig config;
 
-    /*
-    public static final ItemGroup HAT_TAB =
-            FabricItemGroup.builder(new Identifier(SimpleHats.modId))
-                    .displayName(Text.literal("Simple Hats"))
-                    .icon(() -> new ItemStack(ModRegistry.HATICON))
-                    .build();
-     */
+    public static ItemGroup HAT_TAB = FabricItemGroup.builder(new Identifier(modId, "hat_group"))
+            .icon(() -> new ItemStack(ModRegistry.HATICON))
+            .build();
 
     @Override
     public void onInitialize() {
@@ -44,6 +45,30 @@ public class SimpleHats implements ModInitializer {
         */
 
         ModRegistry.registerHats();
+
+        ItemGroupEvents.modifyEntriesEvent(HAT_TAB).register(content -> {
+            content.add(ModRegistry.HATBAG_COMMON);
+            content.add(ModRegistry.HATBAG_UNCOMMON);
+            content.add(ModRegistry.HATBAG_RARE);
+            content.add(ModRegistry.HATBAG_EPIC);
+            content.add(ModRegistry.HATBAG_EASTER);
+            content.add(ModRegistry.HATBAG_SUMMER);
+            content.add(ModRegistry.HATBAG_HALLOWEEN);
+            content.add(ModRegistry.HATBAG_FESTIVE);
+            content.add(ModRegistry.HATSCRAPS_COMMON);
+            content.add(ModRegistry.HATSCRAPS_UNCOMMON);
+            content.add(ModRegistry.HATSCRAPS_RARE);
+            content.add(ModRegistry.HATSCRAPS_EASTER);
+            content.add(ModRegistry.HATSCRAPS_SUMMER);
+            content.add(ModRegistry.HATSCRAPS_HALLOWEEN);
+            content.add(ModRegistry.HATSCRAPS_FESTIVE);
+            content.add(ModRegistry.HATICON);
+            content.add(ModRegistry.HATDISPLAYITEM);
+
+            for(HatItem hat : ModRegistry.hatList) {
+                content.add(hat);
+            }
+        });
 
         FabricDefaultAttributeRegistry.register(ModRegistry.HATDISPLAYENTITY, HatDisplay.createLivingAttributes());
 
