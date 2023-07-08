3d8b9e5ef21a5239cf9a995a00c3ac83693294f6
fonnymunkey <fonnymunkey@gmail.com>
2023-07-08 14:46:48 -0400

    0.2.1
    Fix recursive GLM calls with new Forge update

--- src/main/java/fonnymunkey/simplehats/common/loot/HatEntityLootModifier.java
+++ src/main/java/fonnymunkey/simplehats/common/loot/HatEntityLootModifier.java
@@ -8,6 +8,7 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
@@ -25,7 +26,7 @@ public class HatEntityLootModifier extends LootModifier {
 
     @Override
     public ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> loot, LootContext context) {
-        context.getLevel().getServer().getLootData().getLootTable(INJECTABLE_LOOT).getRandomItems(context, loot::add);
+        context.getLevel().getServer().getLootData().getLootTable(INJECTABLE_LOOT).getRandomItemsRaw(context, LootTable.createStackSplitter(context.getLevel(), loot::add));
         //context.getLootTable(INJECTABLE_LOOT).getRandomItems(context, loot::add);
         return loot;
     }
